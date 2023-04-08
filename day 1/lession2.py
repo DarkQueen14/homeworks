29fe288107c4735c53e1e7f75e69cde198720db3
DarkQueen14 <marishaorshadze14@gmail.com>
2023-04-08 22:39:48 +0300

    second commit

---
+++ day 1/lession2.py
@@ -0,0 +1,46 @@
+name = "mari"
+#name aris cvladi 
+# = aris cvladistvis mnishvnelobis momnichebeli obieqti 
+# "mari" aris cvladistvis mnishvneloba
+
+
+surname = "shaorshadze"
+
+print(name)
+
+#print name
+#print punqciis gadacema ekranze gamosatani obieqti 
+
+name = "mari" #es aris str(string) tipis cvladi 
+age = 24 #ES ARIS INT (integer) mteli ricxvi
+height = 170 #es aris float tipis cvladi (atwiladi)
+#booleon (bool) tipis cvladi
+
+
+knows_programming = True  #true an false
+is_ugly = False # (snakecase) ubralod weris stili standartulad)
+isugly = False #javascripti camelcase
+
+print(name + " " + surname)
+
+#string (str) aris brchyalebshi moqceuli simbolo
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+
+print(name + str(age))
+
+#homework 
+
+print(name + " " + str(surname) + str(age) + str(height))
+
+
+
+
+
+
